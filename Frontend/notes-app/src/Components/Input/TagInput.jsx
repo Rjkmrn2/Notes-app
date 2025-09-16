113f372a7ccd8c8d25e2eb97481b55145a20e9e4
Narman <narmanrk@gmail.com>
2025-09-16 23:05:19 +0530

    Latest changes

--- Frontend/notes-app/src/Components/Input/TagInput.jsx
+++ Frontend/notes-app/src/Components/Input/TagInput.jsx
@@ -2,27 +2,53 @@ import React, { useState } from 'react'
 import { MdAdd, MdClose } from 'react-icons/md';
 
 const TagInput = ({ tags, setTags }) => {
-    const [inpuValue, setInputValue] = useState("")
+    const [inputValue, setInputValue] = useState("")
     const handleInputChange =(e)=>{
         setInputValue(e.target.value);
     }
     const addNewTag = () =>{
         if(inputValue.trim() !== ""){
-            MdSecurityUpdateGood([...tags,setInputValue.trim()])
+            setTags([...tags, inputValue.trim()]);
             setInputValue("");
         }
     }
+    const handleKeyDown = (e) =>{
+        if(e.key=="Enter"){
+            addNewTag();
+        }
+    }
+    const handleRemoveTag = (tagToRemove) =>{
+        setTags(tags.filter((tag)=> tag !== tagToRemove))
+    }
   return (
-    <div>
+    <div className="mb-4">
+        {tags?.length > 0 && (
+        <div className="flex items-centergap-2 flex-wrap mt-2">
+            {tags.map((tag,idx)=>(
+                <span key={idx} className="flex items-center gap-2 text-sm text-slate-900 bg-slate-100 px-3 py-1 rounded">
+                    #{tag}
+                    <button onClick={()=>{handleRemoveTag(tag)}}> 
+                        <MdClose/>
+                    </button>
+                </span>
+            ))}
+        </div>
+        )}
         <div className="flex items-centern gap-4 mt-3">
             <input 
+                value={inputValue}
                 type="text" 
                 className="text-sm bg-transparent border px-3 py-2 rounded outline-none" 
                 placeholder="Add tags"
                 onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
             />
 
-            <button className="w-8 h-8 flex items-center justify-center rounded border border-blue-700 hover:bg-blue-700">
+            <button className="w-8 h-8 flex items-center justify-center rounded border border-blue-700 hover:bg-blue-700"
+                onClick={()=>{
+                    addNewTag();
+                }}
+            >
                 <MdAdd className="text-2xl text-blue-700 hover:text-white"/>
             </button>
         </div>
--- Frontend/notes-app/src/Pages/Home/Home.jsx
+++ Frontend/notes-app/src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from '../../Components/Navbar'
 import NoteCard from '../../Components/Cards/NoteCard'
 import { MdAdd, MdOutlineAlarmAdd } from 'react-icons/md';
 import AddEditNotes from './AddEditNotes'
-
+import Modal from 'react-modal';
 
 const Home = () => {
 
@@ -22,7 +22,7 @@ const Home = () => {
           <NoteCard 
             title="Meeting on 7th April" 
             date="3rd Apr 2024" 
-            content="Meeting on 7th AprilMeeting on 7th April" 
+            content="Meeting on 7t h AprilMeeting on 7th April" 
             tags="#Meeting" 
             isPinned={true} 
             onEdit={()=>{}}
@@ -31,17 +31,12 @@ const Home = () => {
           />
         </div>
       </div>
-
-      <button 
-        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary"
+      
+      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
         onClick={()=>{
           setOpenAddEditModal({isShown: true,type: "add", data:null });
         }}
       >
-        <MdAdd className="text-[32px] text-white"/>
-      </button>
-      
-      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10" onClick={()=>{}}>
         <MdAdd className="text-[32px] text-white"/>
       </button>
 
@@ -55,9 +50,16 @@ const Home = () => {
         }}
         contentLabel=""
         className="w-[40%] max-h-3/4 bg-white mx-auto mt-14 p-5 overflow-scroll"
-      />
+      >
+        <AddEditNotes 
+          type={openAddEditModal.type}
+          noteData={openAddEditModal.data}
+          onClose={()=>{
+            setOpenAddEditModal({isShown:false,type:"add",data:null})
+        }}/>
+      </Modal>
 
-      <AddEditNotes/>
+      
     </>
   )
 }
