692506a25975f5c45b02f379b2a2efa4a40e2d4a
Narman <narmanrk@gmail.com>
2025-11-28 01:17:53 +0530

    Updation

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
-const routes=(
-  <Router>
-    <Routes>
-      <Route path="/dashboard" exaxt element={<Home/>}/>
-      <Route path="/login" exaxt element={<Login/>}/>
-      <Route path="/signup" exaxt element={<SignUp/>}/>
-    </Routes>
-  </Router>
-);
+
 const App = () => {
-  return <div>{routes}</div>
+  return (
+    <Router>
+      <Routes>
+        {/* Default route → Login */}
+        <Route path="/" element={<Login />} />
+
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 };
 
-export default App
+export default App;
